a9589278c023dde5eda2ff5d8bf1ef617ee36718
brainless <bimaarianto19@gmail.com>
2025-08-05 17:11:12 +0700

    Finish feed ui

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -19,7 +19,7 @@ const RecentMessages = () => {
             <h3 className="font-semibold text-slate-8 mb-4">Recent Messages</h3>
             <div className="flex flex-col max-h-56 overflow-y-scroll no-scrollbar">
                 {messages.map((message, index) => (
-                    <Link key={index} className="flex items-start gap-2 py-2 hover:bg-slate-100">
+                    <Link to={`/messages/${message.from_user_id._id}`} key={index} className="flex items-start gap-2 py-2 hover:bg-slate-100">
                         <img src={message.from_user_id.profile_picture} alt="Profile Picture" className="w-8 h-8 rounded-full" />
                         <div className="w-full">
                             <div className="flex justify-between">
